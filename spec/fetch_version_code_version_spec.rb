29f80a4058561b3b35679295760b98fdb14ba41c
AdrieanKhisbe <adriean.khisbe@live.fr>
2021-02-17 15:52:58 +0100

    v0.3.0

--- spec/fetch_version_code_version_spec.rb
+++ spec/fetch_version_code_version_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::FetchVersionCode do
   describe '#VERSION' do
     it 'should returns version' do
-      expect(Fastlane::FetchVersionCode::VERSION).to eq('0.2.1')
+      expect(Fastlane::FetchVersionCode::VERSION).to eq('0.3.0')
     end
   end
 end
